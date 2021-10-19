0efbd3aa870bbdf3e3dfab751b0eb5802d4713e7
Cosmos Appiah <cosmosappiah029@gmail.com>
2021-10-19 03:13:25 +0000

    update

--- src/index.ts
+++ src/index.ts
@@ -73,7 +73,53 @@ inquirer
           );
         },
       },
+      {
+        title: "Install dependencies",
+        task: () => {
+          process.chdir(projectDestination);
+          childProcess.execSync(`${packageManager} install`);
+        },
+      },
+      {
+        title: "Initialize git in directory",
+        task: async () => {
+          const git: SimpleGit = simpleGit({
+            baseDir: projectDestination,
+            binary: "git",
+          });
+          await git.init();
+          await git.add(".");
+          await git.commit("Initialized project using expo-native-create");
+        },
+      },
     ]);
     await tasks.run();
-    console.log(packageManager, projectName, template, projectDestination);
+    const printScript = (script: string): string => {
+      return chalk.cyan`${packageManager} ${script}`;
+    };
+    console.log(
+      chalk.green(`\nSuccess! Created ${projectName} at ${projectDestination}`)
+    );
+    console.log("Inside that directory, you can run several commands:");
+    console.log();
+    console.log(` ${printScript("start")}`);
+    console.log(`   Starts the expo server`);
+    console.log();
+    console.log(` ${printScript("ios")}`);
+    console.log(`   Runs your app in the ios emulator`);
+    console.log();
+    console.log(` ${printScript("android")}`);
+    console.log(`   Runs your app in the android emulator`);
+    console.log();
+    console.log(` ${printScript("web")}`);
+    console.log(`   Runs your app in the browser`);
+    console.log();
+    console.log(` ${printScript("eject")}`);
+    console.log(`   Switch from expo to barebones react-native`);
+    console.log();
+    console.log(
+      `   Compiles the typescript api into javascript for production`
+    );
+    console.log();
+    console.log("Happy hacking!");
   });
